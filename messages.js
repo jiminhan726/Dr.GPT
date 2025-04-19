37d8be566e7e6afb9ac5bcb5c823b58c40a999a4
samtenchodon <schoi197535@gmail.com>
2025-04-19 12:49:51 -0400

    Delete messages.js

--- messages.js
+++
@@ -1,4 +0,0 @@
-const history = [
- { role: "user", content: 
- { role: "assistant", content: "Hi! Thanks for sharing your symptoms. Based on what you said, here are a few possibilities you might want to consider. Please note that I am not a doctor, but I can help you better understand your symptoms:" },
-   
